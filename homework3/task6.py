41d779bac585fa18d0c561b8dc46c5b6886f4de0
Ermolaev Artem <erm_art@mail.ru>
2021-02-12 10:29:51 +0300

    Complete last task from homework3

--- homework3/task6.py
+++ homework3/task6.py
@@ -5,3 +5,24 @@
 # разделенных пробелом. Каждое слово состоит из латинских букв в нижнем регистре.
 # Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 # Необходимо использовать написанную ранее функцию int_func().
+
+def int_func(string):
+    for i in string.split():
+        if i.isalpha():
+            for j in i:
+                if j.islower():
+                    continue
+                else:
+                    return False
+        else:
+            return False
+    print(string.title())
+    return True
+
+
+def main():
+    print('Non compatible format' if int_func(input('Enter string in lower case: ')) is False else '')
+
+
+if __name__ == '__main__':
+    main()
