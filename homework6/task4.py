c159277729debd921c058446baeb26eb0e775935
Artem Ermolaev <erm.art.949@gmail.com>
2021-02-23 23:04:14 +0300

    Tasks 4'th & 5'th from hw6

--- homework6/task4.py
+++ homework6/task4.py
@@ -10,31 +10,89 @@
 # Создайте экземпляры классов, передайте значения атрибутов.
 # Выполните доступ к атрибутам, выведите результат.
 # Выполните вызов методов и также покажите результат.
+import unittest
+
 
 class Car:
-    speed = 0
-    color = ''
-    name = ''
-    is_police = False
 
-    def go(self):
-        pass
-    def stop(self):
-        pass
-    def turn(self, direction):
-        pass
+    def __init__(self, name, color, speed, is_police=False):
+        self.name = name
+        self.color = color
+        self.speed = speed
+        self.is_police = is_police
+
+    @staticmethod
+    def go():
+        return 'Squeal of tires!'
+
+    @staticmethod
+    def stop():
+        return 'brake whistle'
+
+    @staticmethod
+    def turn(direction):
+        return 'turn ' + direction
 
     def show_speed(self):
         return self.speed
 
+
 class TownCar(Car):
-    pass
+    def show_speed(self):
+        if self.speed > 60:
+            print(self.name +' Slow down! Speed limit 60')
+            return 60
+        return self.speed
+
 
 class WorkCar(Car):
-    pass
+    def show_speed(self):
+        if self.speed > 40:
+            print(self.name + ' Slow down! Speed limit 40')
+            return 40
+        return self.speed
+
 
 class SportCar(Car):
     pass
 
+
 class PoliceCar(Car):
     pass
+
+
+class TestCars(unittest.TestCase):
+    def test_move(self):
+        cars = [SportCar('TESLA', 'white', 180),
+                PoliceCar('FORD', 'black', 120, True),
+                WorkCar('FORD', 'dark red', 60),
+                TownCar('Nissan', 'purple', 70)]
+        for car in cars:
+            self.assertEqual(car.go(), 'Squeal of tires!', "Should be 'Squeal of tires!'")
+            self.assertEqual(car.stop(), 'brake whistle', "Should be 'brake whistle'")
+            self.assertEqual(car.turn('right'), 'turn right', "Should be 'turn right'")
+
+    def test_speed(self):
+        sport = SportCar('TESLA', 'white', 180)
+        police = PoliceCar('FORD', 'black', 120, True)
+        work = WorkCar('FORD', 'dark red', 60)
+        town = TownCar('Nissan', 'purple', 70)
+        self.assertEqual(sport.show_speed(), 180, "Should be 180")
+        self.assertEqual(police.show_speed(), 120, "Should be 120")
+        self.assertTrue(police.is_police, "Should be True")
+        self.assertEqual(work.show_speed(), 40, "Should be 40")
+        self.assertEqual(town.show_speed(), 60, "Should be 60")
+
+    def test_rights(self):
+        sport = SportCar('TESLA', 'white', 180)
+        police = PoliceCar('FORD', 'black', 120, True)
+        work = WorkCar('FORD', 'dark red', 60)
+        town = TownCar('Nissan', 'purple', 70)
+        self.assertFalse(sport.is_police, "Should be False")
+        self.assertTrue(police.is_police, "Should be True")
+        self.assertFalse(work.is_police, "Should be False")
+        self.assertFalse(town.is_police, "Should be False")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- homework6/task5.py
+++ homework6/task5.py
@@ -5,3 +5,36 @@
 # В каждом из классов реализовать переопределение метода draw.
 # Для каждого из классов методы должен выводить уникальное сообщение.
 # Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+class Stationary:
+
+    def __init__(self, title="Something draw"):
+        self.title = title
+
+    def draw(self):
+        print(f'Just start drawing! {self.title}')
+
+
+class Pen(Stationary):
+    def draw(self):
+        print(f'Just start drawing with Pen! {self.title}')
+
+
+class Pencil(Stationary):
+    def draw(self):
+        print(f'Just start drawing with Pencil! {self.title}')
+
+
+class Marker(Stationary):
+    def draw(self):
+        print(f'Just start drawing with Marker! {self.title}')
+
+
+stat = Stationary()
+stat.draw()
+
+mark = Marker()
+pen = Pencil()
+
+mark.draw()
+pen.draw()
