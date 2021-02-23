7eda874807599972eca268e58480a17cbd63419c
Artem Ermolaev <erm.art.949@gmail.com>
2021-02-23 22:22:08 +0300

    Complete third task from hw6

--- homework6/task3.py
+++ homework6/task3.py
@@ -10,25 +10,44 @@
 # Проверить работу примера на реальных данных
 # (создать экземпляры класса Position, передать данные,
 # проверить значения атрибутов, вызвать методы экземпляров).
-
+import unittest
 
 
 class Worker:
-    name = ""
-    surname = ""
 
-    class Position:
-        def get_full_name(self, *args):
-            return args[0]+args[1]
-        def get_total_income(self, *args):
-            return sum(args)
-
-    position = Position()
-    __income = {"wage":0, "bonus":0}
-
-    def __init__(self, name, surname, *kwargs):
+    def __init__(self, name, surname, position, wage, bonus):
         self.name = name
         self.surname = surname
-        self.__income = kwargs
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
 
 
+class Position(Worker):
+    def get_full_name(self):
+        return self.name + " " + self.surname
+
+    def get_total_income(self):
+        return sum(self._income.values())
+
+
+class TestPosition(unittest.TestCase):
+    def test_calculate(self):
+        workers = [Position('Thomas', 'Anderson', 'programmer', 10000, 5000),
+                   Position('Elliot', 'Anderson', 'security engineer', 15000, 2000),
+                   Position('Deon', 'Wilson', 'android developer', 13000, 3000)]
+        for val in workers:
+            if val.position == 'programmer':
+                self.assertEqual(val.get_full_name(), 'Thomas Anderson', "Should be Thomas Anderson")
+                self.assertEqual(val.get_total_income(), 15000, "Should be 15.000")
+            elif val.position == 'security engineer':
+                self.assertEqual(val.get_full_name(), 'Elliot Anderson', "Should be Elliot Anderson")
+                self.assertEqual(val.get_total_income(), 17000, "Should be 17.000")
+            elif val.position == 'android developer':
+                self.assertEqual(val.get_full_name(), 'Deon Wilson', "Should be Deon Wilson")
+                self.assertEqual(val.get_total_income(), 16000, "Should be 16.000")
+            else:
+                self.assert_(False, 'Incorrect test data')
+
+
+if __name__ == '__main__':
+    unittest.main()
